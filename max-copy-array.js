89f09de12b0c664148f395166926e3034a7ad3f8
laskor <al.moksud@gmail.com>
2022-02-28 16:32:37 -0500

    max-copy an array done

---
+++ max-copy-array.js
@@ -0,0 +1,12 @@
+
+// max number from an array also using spread operator
+
+const arry = [3, 4, 5, 6, 7, 8, 9];
+
+const max = Math.max(...arry);
+console.log(max);
+
+// copy array
+
+const number = [...arry, 88];
+console.log(number);
