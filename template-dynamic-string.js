21f7b1f5510ba7ffbef426447804a29163a4ac2c
laskor <al.moksud@gmail.com>
2022-02-28 16:51:43 -0500

    dynamic string done

---
+++ template-dynamic-string.js
@@ -0,0 +1,15 @@
+
+// multiple line string using back quote
+
+const str = `first line 
+second line 
+third line 
+fourth `;
+console.log(str);
+
+// dynamic string ${variable name}.
+
+const first = 'mamun';
+const second = 'mama';
+const result = `Name is: ${first} ${second}`;
+console.log(result);
